97ed3f9a3fa19f051097f92fb8c756e32d4f59b1
Vaaiibhav Vaidya <vaaiibhav@techsparagus.com>
2022-02-14 13:40:06 +0530

    added homepage + post loginpage

---
+++ src/pages/PostLogin.js
@@ -0,0 +1,13 @@
+import DeskView from "../components/DeskView";
+import MobileView from "../components/MobileView";
+
+const Profile = () => {
+  return (
+    <>
+      <DeskView>PostLogin page</DeskView>
+      <MobileView>PostLogin page</MobileView>
+    </>
+  );
+};
+
+export default PostLogin;
